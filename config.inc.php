1943a79668ab77ae12a4ed9727883219a69f83b9
Dmitriy <regik@yandex.ru>
2019-11-25 13:55:11 +0700

    Missed file

--- config.inc.php
+++ config.inc.php
@@ -9,7 +9,7 @@ ini_set('display_startup_errors', 1);
 define('CFG_MYSQL_HOST', 'localhost');
 define('CFG_MYSQL_USER', 'root');
 define('CFG_MYSQL_PASS', '');
-define('CFG_MYSQL_DATABASE', 'test');
+define('CFG_MYSQL_DATABASE', 'rabota_test');
 
 /* Initialize Core */
 define('CFG_PATH_DOCUMENT_ROOT', dirname(dirname(dirname(dirname(__FILE__)))) . '/');
